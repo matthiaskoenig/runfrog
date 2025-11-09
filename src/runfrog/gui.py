292d3f6acd14c6c1efd0a4d12dac85a3a8745439
Matthias Koenig <konigmatt@googlemail.com>
2025-11-09 23:23:15 +0100

    working on frog gui

--- src/runfrog/gui.py
+++ src/runfrog/gui.py
@@ -10,7 +10,17 @@ https://fonts.google.com/icons
 """
 
 
-from nicegui import ui
+from nicegui import ui, events
+from pymetadata.console import console
+from starlette.formparsers import MultiPartParser
+
+MultiPartParser.spool_max_size = 1024 * 1024 * 20  # 20 MB
+
+from urllib.parse import urlparse
+
+def is_valid_url(url):
+    result = urlparse(url)
+    return all([result.scheme, result.netloc])
 
 
 @ui.page('/', title='runfrog', favicon="./static/frog.png", dark=False)
@@ -34,13 +44,40 @@ def homepage():
             ui.tab('file', label='Upload File', icon='file_upload')
             ui.tab('url', label='Submit URL', icon='link')
 
-        with ui.tab_panels(tabs, value='file').classes('w-full'):
+        with ui.tab_panels(tabs, value='url').classes('w-full'):
             with ui.tab_panel('file'):
-                ui.label('Select an SBML file with FBC information located on your computer. SBML file or SBML files in a COMBINE archive (OMEX) are supported.')
-                ui.upload(on_upload=lambda e: ui.notify(f'Uploaded {e.file.name}')).classes('w-full')
+                ui.html('Select an <strong>SBML</strong> file with FBC information located on your computer. <br />'
+                        'SBML file or SBML files in a COMBINE archive (OMEX) are supported.', sanitize=False)
+
+                ui.upload(
+                    on_upload=handle_upload,
+                    multiple=False,
+                    max_files=1,
+                ).classes('w-full')
+                ui.button('Create FROG report', on_click=lambda: ui.notify('You clicked me!'))
 
             with ui.tab_panel('url'):
-                ui.label('Type the URL of an SBML file with FBC information. Example: https://github.com/matthiaskoenig/fbc_curation/raw/version-0.2.0/src/fbc_curation/resources/examples/models/e_coli_core.omex')
+                ui.html('Provide the URL of an <strong>SBML file</strong> or <strong>OMEX archive</strong> with FBC information.',
+                        sanitize=False)
+                # markdown = ui.markdown(content="""# Test
+                # This is markdown
+                #
+                # """)
+
+                url_input = ui.input(label='URL', placeholder='start typing',
+                         # on_change=lambda e: result.set_text('you typed: ' + e.value),
+                         validation={
+                             'URL must be valid': lambda value: is_valid_url(value)
+                         },
+                         ).props('clearable').on('keydown.enter', lambda value: console.print(value)).classes('w-full')
+                ui.button('Create FROG report', on_click=lambda: ui.notify(f'You clicked me! Create report for {url_input.value}'))
+
+                ui.html('<h2>Examples</h2>', sanitize=False)
+                ui.html('<code>https://github.com/matthiaskoenig/fbc_curation/raw/version-0.2.0/src/fbc_curation/resources/examples/models/e_coli_core.omex</code>',
+                        sanitize=False)
+                ui.button('Use example',
+                          on_click=lambda: url_input.set_value("https://github.com/matthiaskoenig/fbc_curation/raw/version-0.2.0/src/fbc_curation/resources/examples/models/e_coli_core.omex"))
+
 
     with ui.footer().style('background-color: #3874c8'):
         ui.label('FOOTER')
@@ -71,6 +108,16 @@ def other_page():
 @ui.page('/dark_page', dark=True)
 def dark_page():
     ui.label('Welcome to the dark side')
+
+
+async def handle_upload(e: events.UploadEventArguments):
+    ui.notify(f'Uploaded {e.file.name}')
+    markdown.content = e.file.name
+
+    file: ui.upload.FileUpload = e.file
+    await file.read()
+
+    console.print("Uploaded " + e.file.name)
 
     # with ui.header().classes('bg-blue-500 row items-center') as header:
     #     ui.image('images/mylogo.svg').classes('h-8 mr-4')
